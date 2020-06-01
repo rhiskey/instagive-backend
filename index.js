3870dbfc26d54982dd5dd4215ec5257b7f0fde9b
Vladimir <35545041+rhiskey@users.noreply.github.com>
2020-06-01 13:55:54 +0300

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const mysql      = require('mysql2');
+const cors = require('cors')
+
+
+const connection = mysql.createConnection({
+  host: "77.51.178.66",
+  user: "givawaytest",
+  database: "givaway",
+  password: "uINWTwfn8qUkqup8"
+});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -8,3 +18,20 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  
+app.use(cors());
+// https://expressjs.com/en/guide/routing.html
+module,exports = app.get('/accounts', function (req, res) {
+  connection.connect();
+  connection.query('SELECT * FROM givaway.accounts', function (error, results, fields) {
+    if (error) throw error;
+    res.send(JSON.stringify(results))
+    //console.log(results);
+  });
+  connection.end();
+});
+
+// Start the server
+app.listen(PORT, () => {
+console.log('Go to http://localhost:3001/accounts to see accounts');
+});
