1ea81b5461f7707a96b3e74b62da83f62ad4871f
Rhiskey <first3395@gmail.com>
2021-02-19 19:34:00 +0300

    commit message

--- index.js
+++ index.js
@@ -5,17 +5,13 @@ const mysql = require('mysql2');
 const bodyParser = require("body-parser");
 const cors = require('cors')
 var session = require('express-session');
-// const axios = require('axios')
 const httpRequest = require('request');
 var cookieSession = require('cookie-session')
-// Listen to DataBase Change 
-
 
 const Instagram = require('instagram-web-api')
 const { IG_USERNAME, IG_PASSWORD } = process.env
 const client = new Instagram({ IG_USERNAME, IG_PASSWORD })
 
-// Для парсера
 const Bot = require('./Bot'); // this directly imports the Bot/index.js file
 // const PUPconfig = require('./Bot/config/puppeter.json'); //Need 1 value = hrs to run Scheduler
 
@@ -47,7 +43,6 @@ const run = async (userNickname) => {
 
     console.log(`START TIME - ${startTime} / END TIME - ${endTime}`)
 
-    //Потенциальная утечка, нужно закрыть браузер, но после парсинга
     // await bot.closeBrowser().then(() => console.log("BROWSER CLOSED"));
 
     // instaAccs = bot.getAcc();
@@ -71,45 +66,10 @@ var connection;
 
 connection = mysql.createConnection(db_config);
 
-// создаем парсер для данных application/x-www-form-urlencoded
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-// var dsn = {
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD
-// };
-
-// const instance = new MySQLEvents(dsn, {
-//     startAtEnd: true,
-//     excludedSchemas: {
-//         mysql: true,
-//     },
-// });
-
-// instance.start();
-
-// instance.addTrigger({
-//     name: 'TEST',
-//     expression: '*',
-//     statement: MySQLEvents.STATEMENTS.ALL,
-//     onEvent: (event) => { // You will receive the events here
-//         console.log(event);
-//     },
-// });
-
-// instance.on(MySQLEvents.EVENTS.CONNECTION_ERROR, console.error);
-// instance.on(MySQLEvents.EVENTS.ZONGJI_ERROR, console.error);
-
-// Initialize the app
 const app = express();
 
-//Login Page
-// app.use(session({
-//   secret: '2C44-4D45-FdfpQ38S',
-//   resave: true,
-//   saveUninitialized: true
-// }));
 var expiryDate = new Date(Date.now() + 12 * 60 * 60 * 1000); // 12 hour
 app.use(cookieSession({
     name: 'session',
@@ -124,23 +84,19 @@ app.use(cookieSession({
     }
 }));
 
-//app.use(express.cookieDecoder());
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cors());
-// Parse URL-encoded bodies (as sent by HTML forms)
+
 app.use(express.urlencoded());
 
-// Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 // NEW --------------
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set("view engine", "hbs");
 
-// // СДЕЛАТЬ MVC и API
 // const userController = require("./controllers/userController.js");
 // const followersController = require("./controllers/followersController.js");
 // // определяем Router
@@ -163,7 +119,6 @@ app.set("view engine", "hbs");
 
 // app.set("view engine", "hbs");
 
-// получение списка пользователей
 app.get("/", function (req, res) {
     if (req.session.loggedin) {
         connection.query("SELECT * FROM givaway.mainusers", function (err, data) {
@@ -179,6 +134,7 @@ app.get("/", function (req, res) {
     }
 
 });
+
 
 // //Изменение видимости раздач
 // app.post("/", urlencodedParser, function (req, res) {
@@ -221,8 +177,6 @@ app.post("/create", urlencodedParser, function (req, res) {
 
     if (!req.body) return res.sendStatus(400);
     const sql = "INSERT INTO givaway.mainusers (username, link, giveinfo, avatar, userid) VALUES (?, ?, ?, ?, ?) ";
-    //Generate Unique index 10 symbols
-    //const userID = Date.now(); //1576996323453
     const userID = getRandomIntInclusive(1, 99999);
     var instalink = "https://instagram.com/";
     var link = instalink + req.body.userName;
@@ -240,9 +194,6 @@ app.post("/create", urlencodedParser, function (req, res) {
         // // Дождаться завершения парсинга - передать строку с акками и В БД
 
         res.redirect("/");
-
-        //res.redirect("/");
-        // console.log(results);
     });
 
 });
@@ -285,7 +236,6 @@ app.get("/getfollowers", function (req, res) {
 app.post("/getfollowers", urlencodedParser, function (req, res) {
     if (!req.body) return res.sendStatus(400);
     const username2Parse = req.body.userName;
-    //console.log(req.body);
     //Получить id по нику, если ID есть - > Добавить в БД
     const sql = "SELECT username, userid FROM givaway.mainusers WHERE username = ?";
     connection.query(sql, [username2Parse], function (err, results) {
@@ -295,18 +245,11 @@ app.post("/getfollowers", urlencodedParser, function (req, res) {
             console.log(instaID);
             if (instaID) { //Если есть такой юзер
                 run(username2Parse).catch(e => console.log(e.message));
-
-                // // Дождаться завершения парсинга - передать строку с акками и В БД
-
                 res.redirect("/");
             } else { res.send(500, 'Такого юзера не существует в БД') }
         }
-
-        //run bot at certain interval we have set in our config file
-        // setInterval(run, PUPconfig.settings.run_every_x_hours * 3600000);
     });
 
-    // console.log();
 });
 
 // // возвращаем форму для добавления данных спонсоров
@@ -425,6 +368,7 @@ app.post("/hide/:id", function (req, res) {
         res.redirect("/");
     });
 });
+
 //Очищаем фолловеров
 app.post("/clearfollowed/:userid", function (req, res) {
     // const id = req.params.id;
@@ -584,15 +528,6 @@ app.post("/changeadmin", function (req, res) {
 
 ////-------OLD 
 
-// app.get('/', function (request, response) {
-//   if (request.session.loggedin) {
-//   // response.sendFile(path.join(__dirname + '/login.html'));
-// } else {
-//   response.sendFile(path.join(__dirname + '/login.html'));
-//   // response.send('Пожалуйста авторизируйтесь для просмотра данной страницы!');
-// }
-// // response.end();
-// });
 
 app.post('/auth', function (request, response) {
     var username = request.body.username;
@@ -624,7 +559,6 @@ app.get('/home', function (request, response) {
 });
 
 
-
 // app.get('/mainusers', function (req, res) {
 //     // Проверка авторизации, отправлять POST запрос сюда с данными авторизации
 //     connection.connect();
@@ -642,9 +576,8 @@ app.get('/onlyshow', function (req, res) {
     connection.query('SELECT * FROM givaway.mainusers WHERE showgive = 1', function (error, results, fields) {
         if (error) throw error;
         res.send(JSON.stringify(results))
-        //console.log(results);
     });
-    //connection.end();
+
 });
 
 app.get('/alljoin', function (req, res) {
@@ -653,9 +586,7 @@ app.get('/alljoin', function (req, res) {
     connection.query('SELECT Follow.id, Follow.linkFollower, Follow.avatarFollower, Follow.usernameFollower, mainusers.giveinfo, mainusers.username, mainusers.avatar, mainusers.link FROM Follow INNER JOIN mainusers ON Follow.followedid=mainusers.userid;', function (error, results, fields) {
         if (error) throw error;
         res.send(JSON.stringify(results))
-        //console.log(results);
     });
-    //connection.end();
 });
 
 app.get('/follow', function (req, res) {
@@ -663,9 +594,7 @@ app.get('/follow', function (req, res) {
     connection.query('SELECT * FROM givaway.Follow', function (error, results, fields) {
         if (error) throw error;
         res.send(JSON.stringify(results))
-        //console.log(results);
     });
-    //connection.end();
 });
 
 // получаем данные со фронта и отправляем их в БД - в ответе возвращаем список фолловеров из бд
@@ -735,21 +664,12 @@ app.post("/oauth", urlencodedParser, function (request, responseAuth) {
 });
 
 
-// // Get AuthCode
-// app.post('/oauth', (req, res) => {
-//   if (!req.body) return res.sendStatus(400);
-//   console.log(req.body, "body");
-//   res.send('welcome, ' + req.body.authCode)
-// });
 
 // Start the server
 app.listen(PORT, () => {
     console.log('Go to http://localhost: ${ PORT } 5000 8080/accounts to see accounts');
 });
 
-// curl -X POST \ https://api.instagram.com/oauth/access_token \ -F client_id=296560698030895 \ -F client_secret=759f4d6c839b89130426f21518ca56d5 \ -F grant_type=authorization_code \ -F redirect_uri=https://insta-give.herokuapp.com/ \ -F code={authCode}
-// Отправляем токен обратном во фронт
-// Далее отобразим кнопки для прямой подписки во фронте
 
 app.use(express.static(path.join(__dirname, 'public')));
 
